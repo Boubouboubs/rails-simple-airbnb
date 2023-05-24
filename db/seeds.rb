67bc0c500d4216665c1d2ccdeec00c3228c358b9
stephanie bourdillon <bourdillon.s@gmail.com>
2023-05-24 19:38:49 +0200

    airbnb pas fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Small Flat Limoges',
+  address: '3 avenue Clémenceau',
+  description: 'A tiny flat in a small town',
+  price_per_night: 25,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Gigantic house in Lyon',
+  address: '2 rue principale',
+  description: 'A huge country house outside of Lyon',
+  price_per_night: 115,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'Charming flat in Paris',
+  address: '45 rue des Pyrénées Paris',
+  description: 'A charming flat with typical Parisian decoration',
+  price_per_night: 65,
+  number_of_guests: 5
+)
